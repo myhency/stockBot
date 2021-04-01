8c14eef6596d2fe26e2945f42ff985f8713b0e5c
myhency <hency.yeo@gmail.com>
2021-04-02 01:09:12 +0900

    - 조건식검색 핸들러 추가 - 조건식 모델 추가

--- StockBot/Bot.cs
+++ StockBot/Bot.cs
@@ -13,7 +13,10 @@ namespace StockBot
 {
     public partial class Bot : Form
     {
-        //NLog 이용방법 참고 : https://m.blog.naver.com/PostView.nhn?blogId=sang9151&logNo=221222810693&proxyReferer=https:%2F%2Fwww.google.com%2F
+        /**
+         * NLog 이용방법 참고
+         *  - https://m.blog.naver.com/PostView.nhn?blogId=sang9151&logNo=221222810693&proxyReferer=https:%2F%2Fwww.google.com%2F
+         */
         private static Logger logger = LogManager.GetCurrentClassLogger();
 
         public Bot()
@@ -35,18 +38,26 @@ namespace StockBot
 
         private void axKHOpenAPI1_OnEventConnect(object sender, AxKHOpenAPILib._DKHOpenAPIEvents_OnEventConnectEvent e)
         {
-            if (e.nErrCode == 0) //로그인 성공시
+            if (e.nErrCode == 0)
             {
+                /**
+                 * 로그인 성공시:
+                 *  - DB를 접속해서 어제 수집한 종목들을 gridview에 추가해 준다.
+                 *  - Handler 들을 초기화한다?
+                 */
                 logger.Info("로그인 성공.");
                 var db = connectionFactory();
                 db.Open();
-                logger.Info(db.Database.ToString());
             }
         }
 
         private IDbConnection connectionFactory()
         {
-            //Dapper 이용방법 참고 : https://jacking.tistory.com/1117
+            /**
+             * Dapper 이용방법 참고
+             *  - https://jacking.tistory.com/1117
+             *  - https://github.com/StackExchange/Dapper
+             */
             string ConnString = "server=localhost;port=3306;database=stockdb;uid=root;password=037603";
 
             IDbConnection db = new MySql.Data.MySqlClient.MySqlConnection(ConnString);
---
+++ StockBot/KiwoomAPI/ConditionEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using AxKHOpenAPILib;
+using NLog;
+using StockBot.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockBot.KiwoomAPI
+{
+    public partial class ConditionEventHandler
+    {
+        private static Logger Logger = LogManager.GetCurrentClassLogger();
+        private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
+        private List<Condition> conditionList;
+
+        public ConditionEventHandler(AxKHOpenAPI axKHOpenAPI1)
+        {
+            this.axKHOpenAPI1 = axKHOpenAPI1;
+        }
+    }
+}

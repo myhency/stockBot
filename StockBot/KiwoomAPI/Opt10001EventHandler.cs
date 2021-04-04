6228545f5beaf69e66ad65799d5dd90a805b73c3
myhency <hency.yeo@gmail.com>
2021-04-04 11:10:13 +0900

    VIP-50 구글시트 업데이트 완료

--- StockBot/KiwoomAPI/Opt10001EventHandler.cs
+++ StockBot/KiwoomAPI/Opt10001EventHandler.cs
@@ -16,9 +16,8 @@ namespace StockBot.KiwoomAPI
         private static Logger logger = LogManager.GetCurrentClassLogger();
         private AxKHOpenAPILib.AxKHOpenAPI axKHOpenAPI1;
         private int screenNumber = 1000;
-        private static string mysqlConnStr = "Server=localhost;Database=stockdb;Uid=root;Pwd=037603;Charset=utf8";
-        private MySqlConnection conn = new MySqlConnection(mysqlConnStr);
         private MariaDB db;
+        private GoogleSheet googleSheet;
 
         public Opt10001EventHandler(object sender, AxKHOpenAPI axKHOpenAPI1)
         {
@@ -26,15 +25,7 @@ namespace StockBot.KiwoomAPI
             this.axKHOpenAPI1.OnReceiveTrData += axKHOpenAPI1_OnReceiveTrData;
 
             db = new MariaDB();
-
-            //try
-            //{
-            //    conn.Open();
-            //}
-            //catch (Exception exception)
-            //{
-            //    logger.Error(exception.Message.ToString());
-            //}
+            googleSheet = new GoogleSheet();
         }
 
         public void requestTrOpt10001(string itemCode, string trName)
@@ -57,116 +48,9 @@ namespace StockBot.KiwoomAPI
                 string conditionName = e.sRQName.Split('_')[2];
 
                 db.saveItemInfo(opt10001VO, conditionName);
+                googleSheet.updateCodeListToGoogleSheet(opt10001VO, conditionName);
             }
         }
-
-        //private void saveItemInfo(Opt10001VO opt10001VO, string conditionName)
-        //{
-        //    string query = "INSERT INTO stockdb.searched_items" +
-        //                        "(" +
-        //                        "condition_name," +
-        //                        "item_code," +
-        //                        "item_name," +
-        //                        "settlement_month," +
-        //                        "face_value," +
-        //                        "capital," +
-        //                        "num_of_listed_shares," +
-        //                        "credit_ratio," +
-        //                        "highest_year_round," +
-        //                        "lowest_year_round," +
-        //                        "market_capitalization," +
-        //                        "external_burnout_rate," +
-        //                        "substitute_stock," +
-        //                        "per," +
-        //                        "eps," +
-        //                        "roe," +
-        //                        "pbr," +
-        //                        "bps," +
-        //                        "take," +
-        //                        "operating_profit," +
-        //                        "net_income," +
-        //                        "highest_250," +
-        //                        "lowest_250," +
-        //                        "staring_price," +
-        //                        "highest_price," +
-        //                        "lowest_price," +
-        //                        "upper_limit," +
-        //                        "lower_limit," +
-        //                        "reference_price," +
-        //                        "highest_250_at," +
-        //                        "highest_250_ratio," +
-        //                        "lowest_250_at," +
-        //                        "lowest_250_ratio," +
-        //                        "present_price," +
-        //                        "price_state_flag," +
-        //                        "price_by_prev_day," +
-        //                        "fluctuation_rate," +
-        //                        "volume," +
-        //                        "volume_by_prev_day," +
-        //                        "stocks_in_circulation," +
-        //                        "circulation_ratio," +
-        //                        "created_at)" +
-        //                        " VALUES (" +
-        //                        $"'{conditionName}'," +
-        //                        $"'{opt10001VO.종목코드}'," +
-        //                        $"'{opt10001VO.종목명}'," +
-        //                        $"'{opt10001VO.결산월}'," +
-        //                        $"{opt10001VO.액면가}," +
-        //                        $"{opt10001VO.자본금}," +
-        //                        $"{opt10001VO.상장주식}," +
-        //                        $"{opt10001VO.신용비율}," +
-        //                        $"{opt10001VO.연중최고}," +
-        //                        $"{opt10001VO.연중최저}," +
-        //                        $"{opt10001VO.시가총액}," +
-        //                        $"{opt10001VO.외인소진률}," +
-        //                        $"{opt10001VO.대용가}," +
-        //                        $"{opt10001VO.PER}," +
-        //                        $"{opt10001VO.EPS}," +
-        //                        $"{opt10001VO.ROE}," +
-        //                        $"{opt10001VO.PBR}," +
-        //                        $"{opt10001VO.BPS}," +
-        //                        $"{opt10001VO.매출액}," +
-        //                        $"{opt10001VO.영업이익}," +
-        //                        $"{opt10001VO.당기순이익}," +
-        //                        $"{opt10001VO.최고250}," +
-        //                        $"{opt10001VO.최저250}," +
-        //                        $"{opt10001VO.시가}," +
-        //                        $"{opt10001VO.고가}," +
-        //                        $"{opt10001VO.저가}," +
-        //                        $"{opt10001VO.상한가}," +
-        //                        $"{opt10001VO.하한가}," +
-        //                        $"{opt10001VO.기준가}," +
-        //                        $"STR_TO_DATE('{opt10001VO.최고가일250.ToString("yyyyMMdd")}','%Y%m%d')," +
-        //                        $"{opt10001VO.최고가대비율250}," +
-        //                        $"STR_TO_DATE('{opt10001VO.최저가일250.ToString("yyyyMMdd")}','%Y%m%d')," +
-        //                        $"{opt10001VO.최저가대비율250}," +
-        //                        $"{opt10001VO.현재가}," +
-        //                        $"{opt10001VO.대비기호}," +
-        //                        $"{opt10001VO.전일대비}," +
-        //                        $"{opt10001VO.등락율}," +
-        //                        $"{opt10001VO.거래량}," +
-        //                        $"{opt10001VO.거래대비}," +
-        //                        $"{opt10001VO.유통주식}," +
-        //                        $"{opt10001VO.유통비율}," +
-        //                        $"STR_TO_DATE('{DateTime.Now.ToString("yyyyMMdd")}','%Y%m%d')" +
-        //                        ")";
-
-        //    logger.Debug(query);
-
-        //    MySqlCommand cmd = new MySqlCommand(query, conn);
-        //    try
-        //    {
-        //        cmd.ExecuteNonQuery();
-        //    }
-        //    catch (Exception exception)
-        //    {
-        //        Console.WriteLine(exception.Message.ToString());
-        //    }
-        //    finally
-        //    {
-        //        cmd.Dispose();
-        //    }
-        //}
 
         private Opt10001VO getOpt10001VO(string sTrCode, string sRQName)
         {
---
+++ StockBot/utils/GoogleSheet.cs
@@ -0,0 +1,42 @@
+﻿using NLog;
+using StockBot.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockBot.utils
+{
+    public class GoogleSheet
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        private int rangeRowStart = 2;
+        private string sheetName = "관심종목";
+        public void updateCodeListToGoogleSheet(Opt10001VO opt10001VO, string conditionName)
+        {
+            var gsh = new GoogleSheetsHelper("swing-293507-ca9c2651b2d1.json", "13I2pgJbXZjuqX8EbI3WnNCCuK8j3HpFNbqyAQypEYYg");
+            var row = new GoogleSheetRow();
+            var cells = new List<GoogleSheetCell>();
+
+            cells.Add(new GoogleSheetCell() { CellValue = conditionName });
+            cells.Add(new GoogleSheetCell() { CellValue = DateTime.Now.ToString() });
+            cells.Add(new GoogleSheetCell() { CellValue = opt10001VO.종목명 });
+            cells.Add(new GoogleSheetCell() { CellValue = opt10001VO.종목코드 });
+            cells.Add(new GoogleSheetCell() { CellValue = opt10001VO.시가총액.ToString() });
+            cells.Add(new GoogleSheetCell() { CellValue = opt10001VO.유통주식.ToString() });
+            cells.Add(new GoogleSheetCell() { CellValue = opt10001VO.시가.ToString() });
+            cells.Add(new GoogleSheetCell() { CellValue = opt10001VO.고가.ToString() });
+            cells.Add(new GoogleSheetCell() { CellValue = opt10001VO.저가.ToString() });
+            cells.Add(new GoogleSheetCell() { CellValue = opt10001VO.현재가.ToString() });
+            cells.Add(new GoogleSheetCell() { CellValue = opt10001VO.등락율.ToString() });
+            cells.Add(new GoogleSheetCell() { CellValue = opt10001VO.거래량.ToString() });
+
+            row.Cells.AddRange(cells);
+            var rows = new List<GoogleSheetRow>() { row };
+            gsh.AddCells(new GoogleSheetParameters() { SheetName = sheetName, RangeColumnStart = 1, RangeRowStart = rangeRowStart }, rows);
+            rangeRowStart++;
+            logger.Info($"{opt10001VO.종목명} 업데이트 완료 ({rangeRowStart})");
+        }
+    }
+}
